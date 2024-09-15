03fa7ec79b368523e9b416c5f7e4d138a8c6d718
mokh <mahmouddado555@outlook.com>
2024-09-16 01:32:59 +0300

    adding firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mini_whatsapp/firebase_options.dart';
 
-import 'start/welcome_screen.dart';
+import 'features/start/presentation/screens/welcome_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MiniWhatsApp());
 }
 
